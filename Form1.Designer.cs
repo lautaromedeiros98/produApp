a5ea249d83a7f746aee8ba07b4329a25c6932088
lautaro <lautaromedeiros98@gmail.com>
2021-02-18 21:33:11 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace produApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 42);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(168, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress_1);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(192, 136);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.IO;
+using Clases;
+namespace produApp
+{
+	public partial class Form1 : Form
+	{
+		public ExcelReader reader;
+		private List<Is> _listaIs = new List<Is>();
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			openFileDialog1 = new OpenFileDialog();
+			this.textBox1.Focus();
+				if (openFileDialog1.ShowDialog() == DialogResult.OK)
+				{
+					this.reader = new ExcelReader(openFileDialog1.FileName);
+					this.Cargar();
+				}
+			
+		}
+
+		private void Cargar()
+		{
+			string sr = this.reader.Read(openFileDialog1.FileName);
+			string[] a= sr.Split('\n');
+			string[] varAux;
+			foreach (string aux in a)
+			{
+				if(!string.IsNullOrEmpty(aux))
+				{
+					varAux = aux.Split(',');
+					Is isaux = new Is(varAux[0], varAux[1], float.Parse(varAux[6]), float.Parse(varAux[7]));
+					this._listaIs.Add(isaux);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+
+		private bool mostrarProdu(string IsRe)
+		{
+			foreach(Is aux in this._listaIs)
+			{
+				if(IsRe.Equals(aux.NumeroIs))
+				{
+					if(aux.Unidades<150)
+					{
+						MessageBox.Show("POCA PRODU!!! \nLa is tiene " + aux.Unidades + " unidades");
+					}
+					else
+					{
+						if(aux.Unidades>150 && aux.Unidades<250)
+						{
+							MessageBox.Show("MASO MASO!!! \nLa is tiene " + aux.Unidades + " unidades");
+						}
+						else if(aux.Unidades>250)
+						{
+							MessageBox.Show("BUENA PRODU!!! \nLa is tiene " + aux.Unidades + " unidades");
+							MessageBox.Show("RAU MAQUINA, LAUTICUEVAS GIL");
+						}
+					}
+					this.textBox1.Text = "";
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == (char)13)
+			{
+				if(!this.mostrarProdu(this.textBox1.Text))
+				{
+					MessageBox.Show("La is no existe en el reporte seleccionado");
+				}
+			}
+		}
+	}
+}
